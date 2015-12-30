fb4c595423a4995dc8d4d6346a6c90fbee14ca6f
Jan Rupnik <jan.rupnik@ijs.si>
2015-12-30 09:51:16 +0100

    position of work updated

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 call run_.bat ips-phd-thesis-eng-ieee
-cp ips-phd-thesis-eng-ieee.pdf D:\Dropbox\rupnik-ips-phd-thesis-eng-ieee.pdf
+cp ips-phd-thesis-eng-ieee.pdf D:\Dropbox\phd\rupnik-ips-phd-thesis-eng-ieee.pdf
 echo pause
